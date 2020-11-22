7221b98bd0540bf380f57c1263bb4347800e7da8
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-11-21 18:20:26 -0600

    noot yet

--- balancingParentheses.js
+++ balancingParentheses.js
@@ -1,4 +1,4 @@
-// isBalanced("(Sensei says yes!)", "()")       // => True
+isBalanced("(Sensei says yes!)", "()")       // => True
 isBalanced("(Sensei says no!", "()")         // => False
 // isBalanced("(Sensei [says] yes!)", "()[]")   // => True
 // isBalanced("(Sensei [says) no!]", "()[]") // => False
@@ -13,23 +13,22 @@ function isBalanced(s, caps) {
   }
   //arreglo no para acomodar todo de maneta no enfilada sino  en stack
   let stack = [];
-    //iterar n veces igual a la loongitud del string
+    //separar y hacer arrays
     s = s.split("")
     s.forEach((item, i) => {
-      // item es cada una de las letras del string "s"
-         item = s[i];
-        //si encontramoos que alguna de las letra del string es igual a lo mapeadoo
+        //comparamos cada item con los values del map, para oobtener el signo de ciierre
         if (map[item]) {
-          //lo empujamos al array stack
+          // empujamos signo de cierre al array stack
           stack.push(map[item]);
         } else {
-          if (item !== stack.pop()) {
+          // console.log(item,stack)
+          if (item !== stack[0]) {
+            stack.pop()
             //if item is not equal to the first element of the stack return false
             return false;
           }
         }
     });
-    console.log(stack.length === 0)
     //return the stack when its elements have been exhausted
     return stack.length === 0;
 }
