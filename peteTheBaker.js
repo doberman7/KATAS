de1ffa1d696cb37629c265601bf6ac3346741b89
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-11-22 11:47:49 -0600

    doone peteThe Baker

--- peteTheBaker.js
+++ peteTheBaker.js
@@ -1,7 +1,21 @@
 // must return 2
 // cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200});
 // must return 0
-// cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000});
+cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000});
 function cakes(recipe, available) {
-  // TODO: insert code
+  //cantidad de pasteles
+  let n = [];
+  //iterar por cada ingrediente de la receta
+  for (key in recipe) {
+    //si el ingrediente concuerda con los ingredientes disponibles
+    if (key in available) {
+      //asignar al cociente redondeada hacia abajo de los ingredientes disponibles entre los de la receta
+      let num = Math.floor(available[key] / recipe[key]);
+      //empujar resultado en n
+      n.push(num);
+    } else {
+      return 0;
+    }
+  }
+  return parseInt(n.sort((x, y) => x - y).slice(0, 1)); // where direct return Math.min (n);
 }
