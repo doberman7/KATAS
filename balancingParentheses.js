8200b83e25ff4a2fc432f9c51c6b9210a738f67b
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-11-21 19:33:08 -0600

    done

--- balancingParentheses.js
+++ balancingParentheses.js
@@ -1,34 +1,20 @@
 isBalanced("(Sensei says yes!)", "()")       // => True
 isBalanced("(Sensei says no!", "()")         // => False
-// isBalanced("(Sensei [says] yes!)", "()[]")   // => True
-// isBalanced("(Sensei [says) no!]", "()[]") // => False
-// isBalanced("Sensei says 'yes'!", "''")       // => True
-// isBalanced("Sensei say's no!", "''")         // => False
+
 function isBalanced(s, caps) {
-  //mapear parentesis
-  let map = {
-    "(": ")",
-    "[": "]",
-    "{": "}"
-  }
-  //arreglo no para acomodar todo de maneta no enfilada sino  en stack
-  let stack = [];
-    //separar y hacer arrays
-    s = s.split("")
-    s.forEach((item, i) => {
-        //comparamos cada item con los values del map, para oobtener el signo de ciierre
-        if (map[item]) {
-          // empujamos signo de cierre al array stack
-          stack.push(map[item]);
-        } else {
-          // console.log(item,stack)
-          if (item !== stack[0]) {
-            stack.pop()
-            //if item is not equal to the first element of the stack return false
-            return false;
-          }
-        }
-    });
-    //return the stack when its elements have been exhausted
-    return stack.length === 0;
+  let caps_array = caps.split(""),
+   openers = [".*"],
+   closers = [];
+
+ for(var i=0; i<caps_array.length; i++) {
+   if(i%2==0) {
+     openers.push("[\\"+caps_array[i]+"]")
+     openers.push(".*")
+   } else {
+     closers.push("[\\"+caps_array[i]+"]")
+     closers.push(".*")
+   }
+ }
+ let regex = openers.concat(closers.reverse()).join("");
+ return new RegExp(regex, "g").test(s);
 }
